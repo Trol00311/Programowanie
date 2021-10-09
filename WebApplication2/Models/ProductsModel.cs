b156d907a1654a16c677391f272b4d9138aa8ad1
Tomasz Szewczyk <tomasz.szewczyk1@microsoft.wsei.edu.pl>
2021-10-09 16:25:14 +0200

    add products, models, controllers

---
+++ WebApplication2/Models/ProductsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class ProductsModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set;}
+
+    
+    }
+}
