140a0ee7fb73907ab0f8f6f8c185b0737b74e16c
Tomasz Szewczyk <tomasz.szewczyk1@microsoft.wsei.edu.pl>
2021-11-06 16:21:52 +0100

    add save migrations

--- WebApplication2/Controllers/ProductsController.cs
+++ WebApplication2/Controllers/ProductsController.cs
@@ -3,19 +3,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication2.Database;
+using WebApplication2.Entities;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class ProductsController: Controller
     {
+        private readonly AppDbContext _dbContext;
+
+        public ProductsController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index ()
         {
             return View();
         }
 
-        public IActionResult Add (ProductsModel product)
+        public async Task<IActionResult> Add (ProductsModel product)
         {
+            var entity = new ProductEntity
+            {
+                Name = product.Name,
+                Description = product.Description,
+                IsVisible = product.IsVisible,
+            };
+
+            await _dbContext.Products.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
             var viewModel = new ProductStatsViewModel
             {
                 NameLength = product.Name.Length,
---
+++ WebApplication2/Database/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Entities;
+
+namespace WebApplication2.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ProductEntity> Products {get;set;}
+
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        { 
+        }
+    }
+}
