90ff684f1ccb012c085814eadf5357ad4a302186
Trol00311 <tomasz.szewczyk1@microsoft.wsei.edu.pl>
2021-11-17 15:22:50 +0100

    Download from database and search

--- WebApplication2/Controllers/ProductsController.cs
+++ WebApplication2/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,6 +41,13 @@ namespace WebApplication2.Controllers
                 DescriptionLength = product.Description.Length,
             };
             return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> List()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            return View(products);
         }
 
     }
