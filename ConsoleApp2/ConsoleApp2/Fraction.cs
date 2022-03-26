c4172cced37ee1865c222509ecdb883632f9f03f
Trol00311 <80637774+Trol00311@users.noreply.github.com>
2022-03-26 19:26:46 +0100

    26.03

---
+++ ConsoleApp2/ConsoleApp2/Fraction.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    class Fraction : IComparable<Fraction>
+    {
+
+        public readonly int num;
+        public readonly int den;
+
+        public Fraction(int num, int den)
+        {
+            if (den == 0)
+            {
+                throw new ArgumentException("Mianownik nie moze byc 0");
+            }
+            this.num = num;
+            this.den = den;
+        }
+        public override string ToString()
+        {
+            return $"{num}/{den}";
+        }
+
+        //dodawnie
+        public Fraction add(Fraction secondFraction)
+        {
+            return new Fraction(num * secondFraction.den + secondFraction.num * den,den*secondFraction.den );
+        }
+
+
+
+        //dodawanie
+        public static Fraction operator +(Fraction fr1,Fraction fr2)
+        {
+            return new Fraction(fr1.den * fr2.num + fr1.num * fr2.den, fr1.den * fr2.den);
+        }
+
+        //mnozenie
+        public static Fraction operator *(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(fr1.num * fr2.num, fr1.den * fr2.den);
+        }
+
+        //dzielenie
+        public static Fraction operator /(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(fr1.num * fr2.den, fr1.den * fr2.num);
+        }
+
+        //odejmowanie
+        public static Fraction operator -(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(fr1.den * fr2.num - fr1.num * fr2.den, fr1.den * fr2.den);
+        }
+
+        public int CompareTo(Fraction fr2)
+        {
+            int num1 = num * fr2.den;
+            int num2 = den * fr2.num;
+
+            if(num1>num2)
+            {
+                return 1;
+            }
+            else if(num1<num2)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Compare : IComparer<Fraction>
+    {
+        int IComparer<Fraction>.Compare(Fraction fr1, Fraction fr2)
+        {
+            int num1 = fr1.num * fr2.den;
+            int num2 = fr1.den * fr2.num;
+
+            if (num1 < num2)
+            {
+                return 1;
+            }
+            else if (num1 > num2)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fr1 = new Fraction(1, 3);
+            Fraction fr2 = new Fraction(1, 2);
+            Console.WriteLine(fr1);
+            Console.WriteLine(fr2);
+          
+            // dodawnie
+            Console.WriteLine("Dodawnie: "+fr1.add(fr2));
+            Console.WriteLine("Dodawnie: "+fr1 + fr2);
+
+            //mnozenie
+            Console.WriteLine("Mnozenie: "+fr1 * fr2);
+
+            //dzielenie
+            Console.WriteLine("Dzielenie: "+fr1 / fr2);
+
+            //odejmowanie
+            Console.WriteLine("Odejmowanie: "+(fr1 - fr2));
+
+            List<Fraction> list = new List<Fraction>();
+            list.Add(fr1);
+            list.Add(fr2);
+            list.Add(new Fraction(-3,5));
+            list.Add(new Fraction(2,5));
+
+            foreach(Fraction fr in list)
+            {
+                Console.WriteLine(fr);
+
+            }
+
+            Console.WriteLine("------------------");
+            
+            Console.WriteLine("OD NAJWIEKSZEJ DO NAJMNIJESZEJ: ");
+            list.Sort(new Compare());
+            list.ForEach(Console.WriteLine);
+
+            Console.WriteLine();
+           
+            Console.WriteLine("OD NAJMNIEJSZEJ DO NAJWIEKSZEJ");
+            list.Sort();
+            list.ForEach(Console.WriteLine);
+        }
+    }
+}
